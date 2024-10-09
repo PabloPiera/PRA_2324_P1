7c84b575ec6238fe103149dbe93695fb0a55866e
Pablo Piera Navarro <ppienav@etsinf.upv.es>
2024-10-09 22:41:24 +0200

    Falta el friend y el size()

--- ListArray.h
+++ ListArray.h
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "List.h"
+#include <stdexcept>
 
-template <typename T> 
+using namespace std;
+template <typename T>
+
 class ListArray : public List<T> {
 
     private:
@@ -9,20 +12,35 @@ class ListArray : public List<T> {
 	int max;
 	int n;
 	static const int MINSIZE = 2;
-
+	void resize(int new_size){
+                T *nuevo_array = new T[new_size];
+                for(int i = 0; i < new_size;  i++){
+                        nuevo_array[i] = arr[i];
+                }
+                delete[] arr;
+                arr = nuevo_array;
+                max = new_size;
+        }
     public:
         ListArray(){
-	
+	arr = new T[MINSIZE];
+	max = MINSIZE;
+
 	}
-    	~ListArray(){}
-	T operator[](int pos){}
-	friend std::ostream&operator<<(std::ostream &out, const ListArray<T> &list){}
-	void resize(int new_size){
-		T *nuevo_array = new array[new_size];
-		for(int i = 0; i < new_size;  i++){
-			nuevo_array[i] = arr[i];
-		}
+    	~ListArray(){
 		delete[] arr;
-		arr = nuevo_array;
-		max = new_size
+	}
+	T& operator[](int pos){
+	try{
+		if(pos < 0 || pos > size()-1){
+			throw out_of_range("Posición no valida");
+		}
+	}
+	catch (const out_of_range &e) {
+        	cout << "Excepción capturada: " << e.what() << endl;
+   	}
+	return arr[pos];
+	}
+	friend std::ostream&operator<<(std::ostream &out, const ListArray<T> &list){}
+	
 };
